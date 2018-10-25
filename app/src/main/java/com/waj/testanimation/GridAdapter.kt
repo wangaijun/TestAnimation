13a0ff451af2054d96c4e88a6844c05b70a45622
waj <wangaj@glodon.com>
2018-10-25 14:14:37 +0800

    按钮位置居中

--- app/src/main/java/com/waj/testanimation/GridAdapter.kt
+++ app/src/main/java/com/waj/testanimation/GridAdapter.kt
@@ -12,6 +12,7 @@ import android.widget.ImageView
 
 class GridAdapter(private val context: Context, private val animations: ArrayList<Animation>
 ,private val frame:Int) : RecyclerView.Adapter<Holder>() {
+    val texts = arrayOf("平移动画","缩放动画","旋转动画","透明动画","动画合集","自定义动画")
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): Holder {
         val root = LayoutInflater.from(context).inflate(R.layout.item,p0,false)
         return Holder(root)
@@ -25,24 +26,28 @@ class GridAdapter(private val context: Context, private val animations: ArrayLis
         when(position){
             in animations.indices->{
                 val animation = animations[position]
+                holder.btnPlay.text = texts[position]
                 holder.btnPlay.setOnClickListener {
                     holder.img.startAnimation(animation )
                 }
             }
             6->{
                 holder.img.setImageResource(frame)
+                holder.btnPlay.text = "帧动画"
                 holder.btnPlay.setOnClickListener {
                     val drawable = holder.img.drawable as AnimationDrawable
                     drawable.start()
                 }
             }
             7->{
+                holder.btnPlay.text = "包装器模式"
                 ViewWrapper.performWrapperAnimation(holder.img,10,500)
                 holder.btnPlay.setOnClickListener {
                     ViewWrapper.performWrapperAnimation(holder.img,10,500)
                 }
             }
             8->{
+                holder.btnPlay.text = "插值模式"
                 ValueAnimatorWrapper.performListenerAnimator(holder.img,0,800)
                 holder.btnPlay.setOnClickListener {
                     ValueAnimatorWrapper.performListenerAnimator(holder.img,0,800)
